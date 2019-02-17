8fbc4f3c6033f7258db6d0c928433a584891cea6
wenbo1 <1228925989@qq.com>
2019-02-17 15:49:47 +0800

    commit

--- src/components/resumeView/Profiles.vue
+++ src/components/resumeView/Profiles.vue
@@ -79,6 +79,7 @@ export default {
           }
           .mu-card-actions {
             padding: 0;
+            margin-top: 10px;
             .icon {
               font-size: 30px;
               color: #000;
